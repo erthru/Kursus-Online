019656d4bb9ed7766156a54c7359eae28a90c874
Fal Tohru <ersaka96@gmail.com>
2019-11-28 22:54:59 +0800

    to mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,12 @@
 <html>
 
 <head>
-    <link href="{{ asset('css/app.css') }}" type="text/css" rel="stylesheet" />
-    <script src="{{ asset('js/app.js') }}" type="text/javascript" defer></script>
+    <link href="{{ mix('kursus_online/css/app.css') }}" type="text/css" rel="stylesheet" />
+    <script src="{{ mix('kursus_online/js/app.js') }}" type="text/javascript" defer></script>
 </head>
 
 <body class="bg-white">
     <div id="app">
     </div>
 </body>
-
 </html>
